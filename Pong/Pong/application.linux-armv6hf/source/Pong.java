08dc4fbdd3e8f325fb220086a085ad26f5605db5
TeoXverT <bellatimatteo@gmail.com>
2017-02-28 12:13:15 +0100

    esportazione applicazione win-linux

--- Pong/Pong/application.linux-armv6hf/source/Pong.java
+++ Pong/Pong/application.linux-armv6hf/source/Pong.java
@@ -1,11 +1,27 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Pong extends PApplet {
+
 
 //Pong Game
 
 
 int x =width/2; //posizione pallina
 int y =height/2;
-int vx = 5;   // velocità pallina asse x
-int vy = 2;   //velocità pallina asse y
+int vx = 5;   // velocit\u00e0 pallina asse x
+int vy = 2;   //velocit\u00e0 pallina asse y
 int l=65;     //lunghezza racchette
 int p1  = 0;  // punteggo player 1
 int p2  = 0;  //punteggio player 2
@@ -15,8 +31,8 @@ int inc = 30; // incremento posizione racchette
 PFont f;
 
 
-void setup() {
-  size(700,500);
+public void setup() {
+  
   background(0);
   stroke(255);
   fill(255);
@@ -26,7 +42,7 @@ void setup() {
 
 }
 
-void draw() {
+public void draw() {
   //gameobjects
   background(0);
   line(width/2,0,width/2,height); //linea
@@ -77,14 +93,14 @@ void draw() {
   }  
 }
 
-void resetRacchette(){
+public void resetRacchette(){
   
   r1=250;
   r2=250;
   
  }
 
-void keyPressed() {
+public void keyPressed() {
 
   if (key == 'a') {
     if((r1-inc)>-45)r1 = r1 - inc;
@@ -97,5 +113,15 @@ void keyPressed() {
   } 
   if (key == 'm') {
    if((r2+inc)<height-37) r2 = r2 + inc;
+  }
+}
+  public void settings() {  size(700,500); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Pong" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
   }
 }
